e5e13b71d9ef2f7c3f0936397a3689451e426aad
oussama-dalhi <oussamadalhi00@gmail.com>
2026-06-07 20:54:58 +0100

    bind to socket and listen on it

--- server.c
+++ server.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <string.h>
+
+#define DEFAULT_BUFLEN 4096
+char recvbuffer[DEFAULT_BUFLEN];
 
 int main()
 {
     WSADATA wsaData;
     int result;
-    int version = MAKEWORD(2, 2);
+    WORD version = MAKEWORD(2, 2);
     result = WSAStartup(version, &wsaData);
 
     if (result != 0)
@@ -27,8 +31,74 @@ int main()
 
     struct sockaddr_in saServer;
 
+    memset(&saServer, 0, sizeof(saServer));
     saServer.sin_family = AF_INET;
     saServer.sin_port = htons(8080);
 
-    InetPton(saServer.sin_family, "127.0.0.1", &saServer.sin_addr);
+    int ipResult = InetPton(saServer.sin_family, "127.0.0.1", &saServer.sin_addr);
+
+    if (ipResult != 1)
+    {
+        printf("InetPton failed\n");
+        closesocket(sockfd);
+        WSACleanup();
+        return 1;
+    }
+
+    // Setup the TCP listening socket
+    int resolve = bind(sockfd, (const SOCKADDR *)&saServer, sizeof(saServer));
+
+    if (resolve == SOCKET_ERROR)
+    {
+        printf("bind failed with error: %d\n", WSAGetLastError());
+        closesocket(sockfd);
+        WSACleanup();
+        return 1;
+    }
+
+    // listening for an incoming connection
+    int listenResult = listen(sockfd, SOMAXCONN);
+
+    if (listenResult == SOCKET_ERROR)
+    {
+        printf("listen failed with error: %d\n", WSAGetLastError());
+        closesocket(sockfd);
+        WSACleanup();
+        return 1;
+    }
+
+    // accepting connections from clients
+    struct sockaddr_in clientInfo;
+    memset(&clientInfo, 0, sizeof(clientInfo));
+    int clientInfoSize = sizeof(clientInfo);
+    SOCKET clientSocket = accept(sockfd, (struct sockaddr *)&clientInfo,
+                                 &clientInfoSize);
+
+    if (clientSocket == INVALID_SOCKET)
+    {
+        printf("accept failed: %d\n", WSAGetLastError());
+        closesocket(sockfd);
+        WSACleanup();
+        return 1;
+    }
+
+    // receive and send data on a socket
+    int recieved = recv(clientSocket, recvbuffer, DEFAULT_BUFLEN - 1, 0);
+    if (recieved > 0)
+    {
+        printf("Bytes received: %d\n", recieved);
+        recvbuffer[recieved] = '\0';
+        printf("%s\n", recvbuffer);
+    }
+    else if (recieved == 0)
+    {
+        printf("Connection closing...\n");
+    }
+    else
+    {
+        printf("recv failed: %d\n", WSAGetLastError());
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
 }
