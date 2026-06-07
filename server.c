a606d9c78b7f6e3914b15185c86a29725713ed38
oussama-dalhi <oussamadalhi00@gmail.com>
2026-06-07 04:41:07 +0100

    Create socket

--- server.c
+++ server.c
@@ -9,12 +9,26 @@ int main()
     int version = MAKEWORD(2, 2);
     result = WSAStartup(version, &wsaData);
 
-    if(result != 0)
+    if (result != 0)
     {
         printf("WSAStartup failed with error: %d\n", result);
-        
+
         return 1;
     }
-    
+    // Create socket
+    SOCKET sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (sockfd == INVALID_SOCKET)
+    {
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    struct sockaddr_in saServer;
+
+    saServer.sin_family = AF_INET;
+    saServer.sin_port = htons(8080);
+
+    InetPton(saServer.sin_family, "127.0.0.1", &saServer.sin_addr);
 }
